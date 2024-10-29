77c71335e36745392a3f0f319f41dd920f784f8d
Dunsin Olubobokun <dunsinolubobokun@Dunsins-MacBook-Pro.local>
2024-10-29 16:49:25 +0100

    chore: refactor main.go entry point and db retry logic

--- app/main.go
+++ app/main.go
@@ -27,19 +27,10 @@ func main() {
 		log.Fatal("Unable to load config:", err)
 	}
 
-	// to support some degree of resillence, I implemented a simple retry logic on DB connection
-	// sometimes it might take a few seconds for the DB to be ready. Hence, the reason I implemented a simple retry logic
-	var db *gorm.DB
-	var dbErrx error
-	for retries := 0; retries < 3; retries++ {
-		db, dbErrx = gorm.Open(postgres.Open(conf.DBUrl), &gorm.Config{})
-		if err == nil {
-			break
-		}
-		time.Sleep(2 * time.Second) // wait before retrying
-	}
+	// Open the database connection
+	db, err := gorm.Open(postgres.Open(conf.DBUrl), &gorm.Config{})
 	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", dbErrx)
+		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
 	// run migrations
@@ -47,7 +38,7 @@ func main() {
 		log.Fatalf("Migration failed: %v", err)
 	}
 
-	// initialize the circuit breaker
+	// to support some API resillence as highlighted in the project brief
 	settings := gobreaker.Settings{
 		Name:        "PaymentGateway",
 		MaxRequests: 1,
@@ -74,15 +65,17 @@ func main() {
 	// grouping relevant routes with "api" subrouter and midddleware
 	apiRoutes := router.PathPrefix("/api").Subrouter()
 	apiRoutes.Use(middleware.CircuitBreakerMiddleware(cb))
-	// COULD IMPROVE: for time constraints, ideally this should be a protected route
+	// COULD IMPROVE: ideally this should be a protected routes but limited cos of time constraints,
 	apiRoutes.HandleFunc("/deposit", handler.InitiateDepositHandler).Methods("POST")
 	apiRoutes.HandleFunc("/deposit/verify", handler.VerifyDepositHandler).Methods("POST")
 	apiRoutes.HandleFunc("/withdrawal", handler.WithdrawalHandler).Methods("POST")
-	apiRoutes.HandleFunc("/user/balance", handler.GetUserBalanceHandler).Methods("GET")
+	apiRoutes.HandleFunc("/callback", handler.CallbackHandler).Methods("POST")
 
 	// other routes
 	router.HandleFunc("/health", handler.HealthCheck).Methods("GET")
-	router.HandleFunc("/callback", handler.CallbackHandler).Methods("POST")
+	router.HandleFunc("/api/user/balance", handler.GetUserBalanceHandler).Methods("GET")
+	// api documentation in OpenAPI format (Could alternatively use Swagger docs and annotations above my http handlers)
+	router.HandleFunc("/docs", handler.ApiDocumentation).Methods("GET")
 
 	// Start the server
 	log.Printf("Server started on :%d", conf.HTTPPort)
